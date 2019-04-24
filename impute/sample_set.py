a7404a671d0a41a2f4781615213ed69cb46941a7
Nima Hamidi <nimaa.hamidi@gmail.com>
2019-04-23 17:33:00 -0700

    Implement entry sample-set and improve inheritance for sample-sets

--- impute/sample_set.py
+++ impute/sample_set.py
@@ -1,9 +1,9 @@
 import numpy as np
 import numpy.linalg as npl
 
-from typing import List, Tuple
+from typing import List
 
-from .measurement import Measurement, RowMeasurement
+from .measurement import Measurement, RowMeasurement, EntryMeasurement
 
 
 class SampleSet:
@@ -14,12 +14,18 @@ class SampleSet:
         self.xs: List[Measurement] = []
         self.ys: List[float] = []
 
-    def add_obs(self, x: Measurement, y: float):
-        self.add_all_obs([x], [y])
-
     def add_all_obs(self, xs, ys):
         self.xs.extend(xs)
         self.ys.extend(ys)
+
+        for x, y in zip(xs, ys):
+            self._preprocess_obs(x, y)
+
+    def _preprocess_obs(self, x: Measurement, y: float):
+        pass
+
+    def add_obs(self, x: Measurement, y: float):
+        self.add_all_obs([x], [y])
 
     def value(self, m):
         return np.array([x.sense(m) for x in self.xs])
@@ -48,36 +54,54 @@ class RowSampleSet(SampleSet):
         super().__init__(shape)
 
         self._op_norm = 0.0
-        self.buckets = [[] for _ in range(shape[0])]
-        self.dirty = [False for _ in range(shape[0])]
-        self.row_op_norms = np.zeros(shape[0])
+        self._op_norm_fresh = True
 
-    def add_all_obs(self, xs: List[RowMeasurement], ys: List[float]):
-        super().add_all_obs(xs, ys)
+        self._init_fast_op_norm()
 
-        for x, y in zip(xs, ys):
-            i = x.row_index
+    def _init_fast_op_norm(self):
+        self.buckets = [[] for _ in range(self.shape[0])]
+        self.dirty = [False for _ in range(self.shape[0])]
+        self.row_op_norms = np.zeros(self.shape[0])
 
-            self.buckets[i].append((x, y))
-            self.dirty[i] = True
+    def _preprocess_obs(self, x: RowMeasurement, y: float):
+        i = x.row_index
+        self._op_norm_fresh = False
+        self.dirty[i] = True
+        self.buckets[i].append((x, y))
 
     def op_norm(self):
-
-        if any(self.dirty):
-            self._update_all_row_op_norms()
-            self._op_norm = self.row_op_norms.max()
+        if not self._op_norm_fresh:
+            self._refresh_op_norm()
 
         return self._op_norm
 
-    def _update_all_row_op_norms(self):
+    def _refresh_op_norm(self):
         for i, d in enumerate(self.dirty):
             if d:
-                self._update_row_op_norm(i)
+                self._refresh_row_op_norm(i)
 
-    def _update_row_op_norm(self, i):
+        self._op_norm = self.row_op_norms.max()
+        self._op_norm_fresh = True
+
+    def _refresh_row_op_norm(self, i):
         xis = [x.row_value for x, y in self.buckets[i]]
         xis = np.array(xis)
 
         self.row_op_norms[i] = npl.norm(xis, 2)
 
         self.dirty[i] = False
+
+
+class EntrySampleSet(RowSampleSet):
+
+    def __init__(self, shape):
+        super().__init__(shape)
+
+    def _init_fast_op_norm(self):
+        pass
+
+    def _preprocess_obs(self, x: EntryMeasurement, y: float):
+        self._op_norm = max(self._op_norm, x.entry_value)
+
+    def _refresh_op_norm(self):
+        pass
--- impute/tests/test_sample_set.py
+++ impute/tests/test_sample_set.py
@@ -1,9 +1,8 @@
 import numpy as np
-import numpy.linalg as npl
 import numpy.testing as npt
 
 from impute.measurement import EntryMeasurement
-from impute.sample_set import SampleSet, RowSampleSet
+from impute.sample_set import SampleSet, RowSampleSet, EntrySampleSet
 
 
 def test_sample_set_value():
@@ -109,6 +108,24 @@ def test_row_sample_set_op_norm():
     ys = [i ** 1.5 for i in range(2 * shape[0])]
 
     ss = RowSampleSet(shape)
+    ss.add_all_obs(xs, ys)
+
+    actual = ss.op_norm()
+    expect = max(vals)
+    npt.assert_almost_equal(actual, expect)
+
+
+def test_entry_sample_set_op_norm():
+    shape = 5, 5
+
+    rows = [i for i in range(shape[0])] * 2
+    cols = [j // 2 for j in range(2 * shape[0])]
+    vals = [i ** 0.5 for i in range(shape[0] * 2)]
+
+    xs = [EntryMeasurement(shape, r, c, v) for r, c, v in zip(rows, cols, vals)]
+    ys = [i ** 1.5 for i in range(2 * shape[0])]
+
+    ss = EntrySampleSet(shape)
     ss.add_all_obs(xs, ys)
 
     actual = ss.op_norm()
