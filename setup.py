b5db74af0cd5ce773ab365d1546333f799399a01
Nima Hamidi <nimaa.hamidi@gmail.com>
2019-05-01 17:16:59 -0700

    Add __version__ to the package

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 from setuptools import setup
 
-setup(name='impute',
-      version='0.1',
-      description='Implementation of low-rank imputing methods for general sampling distributions',
-      url='git@github.com:nimily/impute.git',
-      author='Nima Hamidi',
-      author_email='nimaa.hamidi@gmail.com',
-      license='MIT',
-      packages=['impute'],
-      install_requires=['numpy >= 1.16.2',
-                        'scikit-learn >= 0.20.3'],
-      zip_safe=False)
+VERSION = '0.1'
+
+
+def write_version(filename='impute/version.py'):
+    with open(filename, 'w') as f:
+        print(f"version = '{VERSION}'", file=f)
+
+
+def setup_package():
+    write_version()
+    
+    setup(name='impute',
+          version=VERSION,
+          description='Implementation of low-rank imputing methods for general sampling distributions',
+          url='git@github.com:nimily/impute.git',
+          author='Nima Hamidi',
+          author_email='nimaa.hamidi@gmail.com',
+          license='MIT',
+          packages=['impute'],
+          install_requires=['numpy >= 1.16.2',
+                            'scikit-learn >= 0.20.3'],
+          zip_safe=False)
+
+
+if __name__ == '__main__':
+    setup_package()
