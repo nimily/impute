d4588443cceed48eeb4c064469a177b7833a945c
Nima Hamidi <nimaa.hamidi@gmail.com>
2019-05-02 17:42:16 -0700

    Refactor FRSVT fixed-rank tests

--- impute/decomposition/tests/test_randomized_svd.py
+++ impute/decomposition/tests/test_randomized_svd.py
@@ -1,38 +1,33 @@
 import numpy as np
-import numpy.linalg as npl
-import numpy.random as npr
 import numpy.testing as npt
 
-from impute.decomposition.randomized_svd import randomized_svd as svd
+import pytest
+
+from impute.decomposition.randomized_svd import randomized_svd
 
 
-def test():
-    npr.seed(1)
+@pytest.mark.usefixtures('low_rank_matrix')
+class TestRandomizedSvd:
 
-    n = 1500
-    m = 1000
-    r = 10
+    @staticmethod
+    def test_fixed_rank(low_rank_matrix):
+        b, r, eu, ed, ev = low_rank_matrix
 
-    eu = npl.qr(npr.randn(n, r))[0]
-    ed = np.arange(r, 0, -1)
-    ev = npl.qr(npr.randn(m, r))[0]
-    b = eu @ np.diag(ed) @ ev.T
+        au, ad, av = randomized_svd(b, rank=r)
 
-    au, ad, av = svd(b, rank=r)
+        npt.assert_array_almost_equal(ad, ed)
 
-    npt.assert_array_almost_equal(ad, ed)
+        actual = au.T @ au
+        expect = np.eye(actual.shape[0])
 
-    actual = au.T @ au
-    expect = np.eye(actual.shape[0])
+        npt.assert_almost_equal(actual, expect)
 
-    npt.assert_almost_equal(actual, expect)
+        actual = av @ av.T
+        expect = np.eye(actual.shape[0])
 
-    actual = av @ av.T
-    expect = np.eye(actual.shape[0])
+        npt.assert_almost_equal(actual, expect)
 
-    npt.assert_almost_equal(actual, expect)
+        actual = au @ np.diag(ad) @ av
+        expect = b
 
-    actual = au @ np.diag(ad) @ av
-    expect = b
-
-    npt.assert_array_almost_equal(actual, expect)
+        npt.assert_array_almost_equal(actual, expect)
